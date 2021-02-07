078ef22fcd46751ed057976cf01373624f55061a
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2021-02-07 10:22:49 +0800

    my first python program

--- 1.Python_BIG_Picture/03/demos/system/input2.py
+++ 1.Python_BIG_Picture/03/demos/system/input2.py
@@ -3,4 +3,7 @@ import json
 with open('input.json', 'r') as f:
     bharath=json.load(f)
     print(bharath)
-    print(type(bharath))
+    with open('hanuman.txt', 'w') as newfile:
+        newfile.write(bharath['name'] + "'s hobbies are: \n")
+        for hobby in bharath['hobbies']:
+            newfile.write(hobby + "\n")
